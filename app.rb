4832c9c3ef1041795959181e2eeef1ae1f3cf9ab
Adam Fudge <coolfudge@gmail.com>
2017-11-22 00:08:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,41 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      # {"pirate"=>{"name"=>"asdf", "weight"=>"fw", "height"=>"ew", "ships"=>
+      # {"one"=>{"name"=>"rqe", "type"=>"eqw", "booty"=>"gbdf"},
+      # "two"=>{"name"=>"ht", "type"=>"hg", "booty"=>"wetr"}}}}
+      @params = params
+      puts params
+      binding.pry
+      pirate = Pirate.new
+
+      pirate.name = params[:pirates][:name]
+      pirate.weight = params[:pirates][:weight]
+      pirate.height = params[:pirates][:height]
+      @pirate = pirate
+
+      ship1 = Ship.new
+      ship1.name = params[:pirates][:ships].first[:name]
+      ship1.type = params[:pirates][:ships].first[:type]
+      ship1.booty = params[:pirates][:ships].first[:booty]
+      @ship1 = ship1
+
+      ship2 = Ship.new
+      ship2.name = params[:pirates][:ships].last[:name]
+      ship2.type = params[:pirates][:ships].last[:type]
+      ship2.booty = params[:pirates][:ships].last[:booty]
+      @ship2 = ship2
+
+      erb :'pirates/show'
+    end
   end
 end
